3268917e36c043408bf1cb286cfcebc91d34eb66
efajiculay <efajiculay@yahoo.com>
2021-05-17 01:27:47 +0800

    Folder management. Addition of mac installer

---
+++ BioSANS_installer/BioSANS_Mac_Installer/BioSANSLib/BioSANS_installer.sh
@@ -0,0 +1,14 @@
+xcode-select —install
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew install python
+python3 -m pip install --upgrade pip
+pip install pillow
+pip install numpy
+pip install matplotlib==3.3.3
+pip install simps
+pip install spicy
+pip install sdeint
+pip install func_timeout
+pip install python-libsbml
+Brew install python-tk
+python3 -m pip install -i https://test.pypi.org/simple/ BioSANS2020-efajiculay
