229d3c31645b1f052b62420c2f7448fbc71c9aa2
efajiculay <efajiculay@yahoo.com>
2021-12-14 08:16:52 +0800

    update pep8

--- BioSANS/src/BioSANS2020/propagation/deterministic/LNAfunctionOfTime.py
+++ BioSANS/src/BioSANS2020/propagation/deterministic/LNAfunctionOfTime.py
@@ -1,14 +1,16 @@
-#import sys
-#import os
+# import sys
+# import os
 # sys.path.append(os.path.abspath("BioSANS2020"))
 
+import numpy as np
 from scipy.integrate import odeint
 from BioSANS2020.propagation.propensity import propensity_vec, \
     propensity_vec_molar
 from BioSANS2020.propagation.recalculate_globals import get_globals
-from BioSANS2020.propagation.deterministic.lna_approx import lna_ss_jacobian, lna_model_ss
+from BioSANS2020.propagation.deterministic.lna_approx \
+    import lna_ss_jacobian, lna_model_ss
 from BioSANS2020.myglobal import mglobals as globals2
-import numpy as np
+
 
 
 def LNA_ode_model(z, t, Sp, ks_dict, r_dict, p_dict, V, molar=False):
@@ -31,10 +33,13 @@ def LNA_cov_model(A, B, C):
 
 
 def LNA_non_steady_state_old(
-        conc, t, Sp, ks_dict, r_dict, p_dict, V, molar=True, rfile="", del_coef=10):
+        conc, t, Sp, ks_dict, r_dict, p_dict, V,
+        molar=True, rfile="", del_coef=10):
     get_globals(rfile)
     z = [conc[a] for a in Sp]
-    zoftime = odeint(LNA_ode_model, z, t, args=(Sp, ks_dict, r_dict, p_dict, V, molar))
+    zoftime = odeint(
+        LNA_ode_model, z, t,
+        args=(Sp, ks_dict, r_dict, p_dict, V, molar))
 
     half = []
     SiNew = []
@@ -57,7 +62,8 @@ def LNA_non_steady_state_old(
         for sp in Sp:
             conc[sp] = stch_var[ind]
             ind = ind + 1
-        AA = lna_ss_jacobian(lna_model_ss, stch_var, Sp, V, ks_dict, r_dict, p_dict)
+        AA = lna_ss_jacobian(
+            lna_model_ss, stch_var, Sp, V, ks_dict, r_dict, p_dict)
         f = propensity_vec_molar(ks_dict, conc, r_dict, p_dict, True)
         BB = np.matmul(np.matmul(V, np.diag(f.flatten())), V.T)
 
@@ -75,7 +81,8 @@ def LNA_non_steady_state(conc, t, Sp, ks_dict, r_dict, p_dict, V,
                          molar=True, rfile="", del_coef=10):
     get_globals(rfile)
     z = [conc[a] for a in Sp]
-    #zoftime = odeint(LNA_ode_model,z,t, args=(Sp,ks_dict,r_dict,p_dict,V,molar))
+    # zoftime = odeint(
+    #     LNA_ode_model,z,t, args=(Sp,ks_dict,r_dict,p_dict,V,molar))
 
     half = []
     SiNew = []
@@ -100,7 +107,8 @@ def LNA_non_steady_state(conc, t, Sp, ks_dict, r_dict, p_dict, V,
         for sp in Sp:
             conc[sp] = stch_var[ind]
             ind = ind + 1
-        AA = lna_ss_jacobian(lna_model_ss, stch_var, Sp, V, ks_dict, r_dict, p_dict)
+        AA = lna_ss_jacobian(
+            lna_model_ss, stch_var, Sp, V, ks_dict, r_dict, p_dict)
         f = propensity_vec_molar(ks_dict, conc, r_dict, p_dict, True)
         BB = np.matmul(np.matmul(V, np.diag(f.flatten())), V.T)
 
@@ -108,7 +116,8 @@ def LNA_non_steady_state(conc, t, Sp, ks_dict, r_dict, p_dict, V,
         B = np.nan_to_num(BB)
 
         fxCovr = LNA_cov_model(A, B, C)
-        fxConc = LNA_ode_model(stch_var, t, Sp, ks_dict, r_dict, p_dict, V, molar)
+        fxConc = LNA_ode_model(
+            stch_var, t, Sp, ks_dict, r_dict, p_dict, V, molar)
 
         h1 = np.abs(fxCovr) + 1.0e-30
         dt = max(min(del_coef * np.min(1 / h1), dt), 1.0e-4)
@@ -125,7 +134,8 @@ def LNA_non_steady_state2(conc, t, Sp, ks_dict, r_dict, p_dict, V,
     z = [conc[a] for a in Sp]
     Cov, slabels, tnew = LNA_non_steady_state(
         conc, t, Sp, ks_dict, r_dict, p_dict, V, molar, rfile, del_coef)
-    zoftime = odeint(LNA_ode_model, z, tnew, args=(Sp, ks_dict, r_dict, p_dict, V, molar))
+    zoftime = odeint(
+        LNA_ode_model, z, tnew, args=(Sp, ks_dict, r_dict, p_dict, V, molar))
 
     SiNew = []
     Sps = [s for s in Sp]
--- BioSANS/src/BioSANS2020/propagation/deterministic/lna_approx.py
+++ BioSANS/src/BioSANS2020/propagation/deterministic/lna_approx.py
@@ -1,3 +1,29 @@
+"""
+
+                     This is the lna_approx module
+
+     This module performs numerical linear noise approximation or LNA by
+exploiting the following relationship.
+
+                         AC + CA.T + BB = 0
+
+where A is defined as d(S*f)/dx where S is the stoichiometric  matrix, f
+are the propensities or fluxes, and x are the components or species. The
+flux is a function of  x and rate constant k. BB is the diffusion matrix
+equivalent  to S*diag(f)*S.T where diag(f) is a  square matrix with zero
+non-diagonal elements and f[i] in each diagonal elements.
+
+The following are the list of function for this module.
+
+1. rem_rowcol_zero
+2. lna_ss_jacobian
+3. lna_model_ss
+4. lna_steady_state
+
+
+"""
+
+
 # import sys
 # import os
 # sys.path.append(os.path.abspath("BioSANS2020"))
@@ -11,11 +37,76 @@ from BioSANS2020.gui_functs.scrollable_text import prepare_scroll_text
 
 
 def rem_rowcol_zero(a_mat):
+    """This function removes rows and columns without non-zero entries.
+
+    Args:
+        a_mat (np.ndarray): numpy matrix of A or d(S*f)/dx as described
+            in the module docstring.
+
+    Returns:
+        np.ndarray: numpy matrix with no rows and columns without non-
+            zero entries.
+    """
     return a_mat[:, ~np.all(a_mat == 0, axis=0)][~np.all(a_mat == 0, axis=1)]
 
 
 def lna_ss_jacobian(model, zlist, sp_comp, stch_var,
                     ks_dict, r_dict, p_dict):
+    """This  function calculataes  the  jacobian  of  the model ODE with
+    respect to the list of species concentration zlist.
+
+    Args:
+        model (function): the ODE model returning derivative of species
+            or components as a function of time
+        zlist (list): list of components or species amounts
+        sp_comp (dict): dictionary of appearance or position of species
+            or component in the reaction tag of BioSANS topology file.
+
+            For example;
+
+                #REACTIONS
+                A => B, -kf1    # negative means to be estimated
+                B => C, kf2
+
+            The value of sp_comp is
+
+                sp_comp = {'A': {0}, 'B': {0, 1}, 'C': {1}}
+
+                A appears in first reaction with index 0
+                B appears in first and second reaction with index 0, 1
+                C appears in second reaction with index 1
+        stch_var (np.ndarray): stoichiometric matrix
+        ks_dict (dict): dictionary of rate constant that appear in each
+            reactions.
+
+            For example;
+
+                #REACTIONS
+                A => B , 0.3        # first reaction
+                B <=> C, 0.1, 0.2   # second reaction
+
+            The value of ks_dict is
+
+                ks_dict = {
+                    0 : [0.3],      # first reaction
+                    1 : [0.1, 0.2]  # second reaction
+                }
+        r_dict (dict): dictionary of reactant stoichiometry. For example
+
+            r_dict = {
+                0: {'A': 1},  # first reaction, coefficient of A is 1
+                1: {'B': 1}   # second reaction, coefficient of B is 1
+            }
+        p_dict (dict): dictionary of product stoichiometry. For example
+
+            p_dict = {
+                0: {'B': 1},  # first reaction, coefficient of B is 1
+                1: {'C': 1}   # second reaction, coefficient of C is 1
+            }
+
+    Returns:
+        np.ndarray: jacobian matrix or d(S*f)/dx or A
+    """
     jacbn = np.zeros((len(zlist), len(zlist)))
     div = 1
     new = 1000
@@ -42,6 +133,59 @@ def lna_ss_jacobian(model, zlist, sp_comp, stch_var,
 
 
 def lna_model_ss(zlist, sp_comp, ks_dict, r_dict, p_dict, stch_var):
+    """This  function returns the derivative of components with respect
+    to time at a particular state of the system based on the inputs.
+
+    Args:
+        zlist (list): list of components or species amounts
+        sp_comp (dict): dictionary of appearance or position of species
+            or component in the reaction tag of BioSANS topology file.
+
+            For example;
+
+                #REACTIONS
+                A => B, -kf1    # negative means to be estimated
+                B => C, kf2
+
+            The value of sp_comp is
+
+                sp_comp = {'A': {0}, 'B': {0, 1}, 'C': {1}}
+
+                A appears in first reaction with index 0
+                B appears in first and second reaction with index 0, 1
+                C appears in second reaction with index 1
+        ks_dict (dict): dictionary of rate constant that appear in each
+            reactions.
+
+            For example;
+
+                #REACTIONS
+                A => B , 0.3        # first reaction
+                B <=> C, 0.1, 0.2   # second reaction
+
+            The value of ks_dict is
+
+                ks_dict = {
+                    0 : [0.3],      # first reaction
+                    1 : [0.1, 0.2]  # second reaction
+                }
+        r_dict (dict): dictionary of reactant stoichiometry. For example
+
+            r_dict = {
+                0: {'A': 1},  # first reaction, coefficient of A is 1
+                1: {'B': 1}   # second reaction, coefficient of B is 1
+            }
+        p_dict (dict): dictionary of product stoichiometry. For example
+
+            p_dict = {
+                0: {'B': 1},  # first reaction, coefficient of B is 1
+                1: {'C': 1}   # second reaction, coefficient of C is 1
+            }
+        stch_var (np.ndarray): stoichiometric matrix
+
+    Returns:
+        np.ndarray: derivative of species with respect to time.d(S*f)/dt
+    """
     conc = {}
     ind = 0
     for spi in sp_comp:
@@ -55,6 +199,66 @@ def lna_model_ss(zlist, sp_comp, ks_dict, r_dict, p_dict, stch_var):
 
 def lna_steady_state(t_var, sp_comp, ks_dict, conc, r_dict, p_dict,
                      stch_var, items=None):
+    """[summary]
+
+    Args:
+        t_var (list): time stamp
+       sp_comp (dict): dictionary of appearance or position of species
+            or component in the reaction tag of BioSANS topology file.
+
+            For example;
+
+                #REACTIONS
+                A => B, -kf1    # negative means to be estimated
+                B => C, kf2
+
+            The value of sp_comp is
+
+                sp_comp = {'A': {0}, 'B': {0, 1}, 'C': {1}}
+
+                A appears in first reaction with index 0
+                B appears in first and second reaction with index 0, 1
+                C appears in second reaction with index 1
+        ks_dict (dict): dictionary of rate constant that appear in each
+            reactions.
+
+            For example;
+
+                #REACTIONS
+                A => B , 0.3        # first reaction
+                B <=> C, 0.1, 0.2   # second reaction
+
+            The value of ks_dict is
+
+                ks_dict = {
+                    0 : [0.3],      # first reaction
+                    1 : [0.1, 0.2]  # second reaction
+                }
+        conc (dict): dictionary of initial concentration.
+
+            For example;
+
+                {'A': 100.0, 'B': -1.0, 'C': 0.0}
+                negative means unknown or for estimation
+        r_dict (dict): dictionary of reactant stoichiometry. For example
+
+            r_dict = {
+                0: {'A': 1},  # first reaction, coefficient of A is 1
+                1: {'B': 1}   # second reaction, coefficient of B is 1
+            }
+        p_dict (dict): dictionary of product stoichiometry. For example
+
+            p_dict = {
+                0: {'B': 1},  # first reaction, coefficient of B is 1
+                1: {'C': 1}   # second reaction, coefficient of C is 1
+            }
+        stch_var (np.ndarray): stoichiometric matrix
+
+        items (tuplel): (canvas, scroll_x, scroll_y). Defaults to None.
+
+    Returns:
+        [type]: [description]
+    """
     ind = 0
     zlist = []
     for spi in sp_comp:
