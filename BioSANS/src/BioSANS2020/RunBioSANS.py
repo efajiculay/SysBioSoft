ebd66cc42ef7622794cc9144de8b6ceea43ad3ca
efajiculay <efajiculay@yahoo.com>
2021-12-17 09:29:30 +0800

    pep8

--- BioSANS/src/BioSANS2020/RunBioSANS.py
+++ BioSANS/src/BioSANS2020/RunBioSANS.py
@@ -1,11 +1,25 @@
+"""
+
+                  This module is the RunBioSANS module
+
+This can be used to run BioSANS and BioSSL byb running the folloeing.
+
+BioSSL()
+BioSANS()
+
+
+"""
+
 from os import system
 from subprocess import Popen
 from sys import executable
 
 
 def BioSANS():
+    """This function launched BioSANS"""
     Popen([str(executable), "-m", "BioSANS2020.BioSANS"])
 
 
 def BioSSL():
+    """This function launched BioSSL"""
     system(str(executable) + " -m BioSANS2020.BioSSL")
