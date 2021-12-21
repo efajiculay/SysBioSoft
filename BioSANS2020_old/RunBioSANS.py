3c01d72f204281242fdfecd1e0e955050af1ab64
efajiculay <efajiculay@yahoo.com>
2021-12-21 13:13:52 +0800

    delete_old

--- BioSANS2020_old/RunBioSANS.py
+++
@@ -1,9 +0,0 @@
-from os import system
-from subprocess import Popen
-from sys import executable
-
-def BioSANS():
-	Popen(str(executable)+" -m BioSANS2020.BioSANS")
-
-def BioSSL():
-	system(str(executable)+" -m BioSANS2020.BioSSL")
