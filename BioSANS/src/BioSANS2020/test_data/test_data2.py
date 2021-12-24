407f44c26a775e2532a263e8e4c7aafb09a8fbad
efajiculay <efajiculay@yahoo.com>
2021-12-24 15:25:25 +0800

    now

--- BioSANS/src/BioSANS2020/test_data/test_data2.py
+++ BioSANS/src/BioSANS2020/test_data/test_data2.py
@@ -5,9 +5,13 @@ This module is for initial test
 """
 
 
-from numpy import linspace, sin, cos, pi, vstack
-label = ["Thank you for using BioSANS"]
+from numpy import linspace, sin, cos, pi, vstack, random
+label = "Thank you for using BioSANS".split()
 theta = linspace(0, 2 * pi, 100)
 x = 16 * (sin(theta) ** 3)
-y = 13 * cos(theta)-5*cos(2*theta)-2*cos(3*theta)-cos(4*theta)
-data = [vstack((x, y)).T]
+tup = (x,)
+for i in range(5):
+    y = 13 * cos(theta)-5*cos(2*theta)-2*cos(3*theta)-cos(4*theta) \
+        + 5*random.rand()
+    tup = tup + (y,)
+data = [vstack(tup).T]
