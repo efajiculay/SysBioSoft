cfeed3d0ac244d83ad8844d8987cf2bcfd726fd6
efajiculay <efajiculay@yahoo.com>
2021-12-30 06:17:36 +0800

    now updated libbiosans

--- BioSANS/setup.py
+++ BioSANS/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 	name="BioSANS2020",
-	version="0.2.8",
+	version="0.2.9",
     author="Erickson Erigio Fajiculay",
     author_email="efajiculay@yahoo.com",
     description="Symbolic and Numeric Software for Systems Biology",
